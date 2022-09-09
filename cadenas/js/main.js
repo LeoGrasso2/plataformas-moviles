f5dbd7d16a7c3982d2711a37986333e8c5fe9e2b
LeoGrasso2 <grassomleo@gmail.com>
2022-09-09 14:04:09 -0300

    Creada carpeta para el trabajo de 'cadenas' y archivos

---
+++ cadenas/js/main.js
@@ -0,0 +1,3 @@
+// Redactá y resolvé un ejercicio que sea coherente, en el que puedas aplicar lo aprendido de cadenas hasta el momento
+
+// Nota: El ejercicio de métodos de array + este son el TP 1 y llevan nota numérica.
